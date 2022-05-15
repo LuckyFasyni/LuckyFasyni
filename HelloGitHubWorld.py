5ee51bb0188ae5bfa84d3ad674280a53d0b99aa3
Achmad Luckyta Fasyni <47514274+LuckyFasyni@users.noreply.github.com>
2022-05-15 07:12:42 +0700

    create first edit in a new branch

--- HelloGitHubWorld.py
+++ HelloGitHubWorld.py
@@ -2,3 +2,5 @@ print("Hello GitHub World!!!")
 print("This is my first ever code committed here in GitHub")
 print("I want to try some things, like \ncreating new files, \ncommitting files, \ncreating a branch")
 print("Please enjoy")
+
+print("I'm in an alternate universe!!!")
