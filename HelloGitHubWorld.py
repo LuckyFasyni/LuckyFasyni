da607ab32959313dfbf509017d36d8c3296e8864
Achmad Luckyta Fasyni <47514274+LuckyFasyni@users.noreply.github.com>
2022-05-15 07:10:03 +0700

    create first py file

---
+++ HelloGitHubWorld.py
@@ -0,0 +1,4 @@
+print("Hello GitHub World!!!")
+print("This is my first ever code committed here in GitHub")
+print("I want to try some things, like \ncreating new files, \ncommitting files, \ncreating a branch")
+print("Please enjoy")
